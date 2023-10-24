171b6aa4ed0b8580d5a9eb10feaa44fca0faeb26
Frank Dillon <dillonf2@outlook.com>
2023-10-23 20:09:27 -0400

    validation flask lab complete

--- server/models.py
+++ server/models.py
@@ -15,6 +15,18 @@ class Author(db.Model):
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
+    @validates('name')
+    def validate_author(self, key, address):
+        if address == '':
+            raise ValueError("Authors must have a name.")
+        return address
+
+    @validates('phone_number')
+    def validate_phone_number(self, key, address):
+        if len(address) != 10:
+            raise ValueError("Phone numbers must be 10 digits long.")
+        return address
+
 class Post(db.Model):
     __tablename__ = 'posts'
     # Add validations and constraints 
@@ -30,3 +42,32 @@ class Post(db.Model):
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
+
+    @validates('title')
+    def validate_title(self, key, title):
+        if title == '':
+            raise ValueError("Posts must have a title.")
+
+        clickbait_keywords = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(keyword in title for keyword in clickbait_keywords):
+            raise ValueError("Title must contain at least one clickbait keyword.")
+        return title
+        
+    @validates('content')
+    def validate_content(self, key, address):
+        if len(address) < 250:
+            raise ValueError("Content must be at least 250 characters long.")
+        return address
+
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if len(summary) >= 250:
+            raise ValueError("Post summaries must be shorter than 250 characters.")
+        return summary
+    
+    @validates('category')
+    def validate_category(self, key, address):
+        if address != "Fiction" and address != "Non-Fiction":
+            raise ValueError("Post category must be either Fiction or Non-Fiction.")
+        return address
+
